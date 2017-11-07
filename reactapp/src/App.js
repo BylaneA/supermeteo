e875b7cc3ca9ae1e93033cdb56031db6d95e9ae0
Bilane A <billieabdiw@gmail.com>
2017-11-07 16:08:58 +0100

    Affichage des infos en retard

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -10,32 +10,50 @@ class App extends Component {
         city: 'Rennes',
         temp: ''
       };
+      this.goToThisCity = this.goToThisCity.bind(this);
+      this.fonctionQuiFetch = this.fonctionQuiFetch.bind(this)
     }
 
-    componentWillMount() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Rennes&lang=fr&units=metric&appid=dfaee76e889bd1d232719d5b18c12009',{
+
+  fonctionQuiFetch() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+this.state.city+"&lang=fr&units=metric&appid=dfaee76e889bd1d232719d5b18c12009",{
         mode: 'cors'
       })
         .then(res => res.json())
         .then(data => {
+          console.log(data)
           const temp = data.main.temp;
           this.setState({temp});
         });
-    }
-  
+  }
+  componentWillMount() {
+      this.fonctionQuiFetch();
+  }
+  goToThisCity(event) {
+    event.preventDefault();
+    const city = this.cityInput.value;
+
+    this.setState({city});
+    this.fonctionQuiFetch();
+  };
   render() {
 
     return (
 
         <div className="App-intro"> 
 
-          <div className="City">
-            {this.state.city}
-          </div>
+          <form className="Formulaire" onSubmit={(e) => this.goToThisCity(e)}>
 
-          <div className= "Temp">
+            <input type="text" placeholder="Ville" required ref={(input) => {this.cityInput = input}} />
+           
+            <button type="submit">Let's see</button>
+
+          </form> 
+
+          <div className="Temperature">
             {this.state.temp}
           </div>
+
         </div>
 
     );
